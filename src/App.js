1aaa2431caf1738440b383931c7d271b6804c526
airton <airtonfrance@gmail.com>
2020-08-01 07:08:35 -0400

    Adição do conteúdo inicial

--- src/App.js
+++ src/App.js
@@ -13,11 +13,22 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"Notas de Atualização 10.15 - Análise Completa"}
+        videoDescription={"Análise do Campeão"}
       />
       <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
+        category={dadosIniciais.categorias[1]}
+      />
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />
+      <Carousel
+        category={dadosIniciais.categorias[5]}
       />
       <Footer />
     </div>
