7f3bbc166d79c400fe5b2a5f1e7105782f0b44f8
airton <airtonfrance@gmail.com>
2020-08-04 11:21:27 -0400

    Atribuindo rotas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,15 +3,16 @@ import Icon from '../../assets/img/Icon.jpg';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
+import { Link } from 'react-router-dom';
 
 function Menu() {
     return(
         <nav className="Menu">
-        <a href="/">
+        <a to="/">
             <img class="Logo" src={Logo} alt="Djokolol logo" />
             <img class="Icon" src={Icon} alt="Djokolol icon" />
         </a>
-        <Button className="ButtonLink" href="/">
+        <Button as={Link}className="ButtonLink" to="/cadastro/video">
             Novo Vídeo
         </Button>
         </nav>
